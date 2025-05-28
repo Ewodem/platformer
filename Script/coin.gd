a6e5325bde5f9d856c50c3666af4ecae505254a9
Ewoud Walter Antoon Demeestere <ewodem@elevad.umea.se>
2025-05-28 10:41:24 +0200

    platformer update

--- Script/coin.gd
+++ Script/coin.gd
@@ -4,11 +4,11 @@ const ROTATION_SPEED = 0.05 # Speed at which the coin rotates
 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Rotates coin
 func _process(_delta: float) -> void:
 	rotate_y(ROTATION_SPEED)
 
-
+#Pcik up coin
 func _on_body_entered(_body: Node3D) -> void:
 	$"../../Player".jump_velocity = $"../../Player".jump_velocity*Globals.jump_mod
 	$"../../Label2".coins_collected += 1
